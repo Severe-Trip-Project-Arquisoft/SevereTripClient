86c3f00cf92ffc0dc19ba3d870313a8d87dac2ef
daniel <daapinzonch@unal.edu.co>
2019-11-08 17:54:12 -0500

    login JWT full

--- src/main/java/com/user/web/security/JwtProperties.java
+++ src/main/java/com/user/web/security/JwtProperties.java
@@ -2,7 +2,7 @@ package com.user.web.security;
 
 public class JwtProperties {
     public static final String SECRET = "ALV";
-    public static final int EXPIRATION_TIME = 5*60*60;
+    public static final int EXPIRATION_TIME = 5*60*60*1000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
